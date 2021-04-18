730a8c230e4e81b4acd39a6fa93270df1b210366
Annalise-M <nali247@gmail.com>
2021-04-18 04:49:46 -0700

    passing snap test

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
