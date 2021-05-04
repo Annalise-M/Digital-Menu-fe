cfdccd2bc4f76f9e859843405b6fef6e511bcdc3
Annalise-M <nali247@gmail.com>
2021-05-03 18:36:26 -0700

    adding some more scss

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import App from './App';
+import App3 from './App3';
 
-describe('App component', () => {
+describe('App3 component', () => {
   afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
+  it('renders App3', () => {
+    const { asFragment } = render(<App3 />);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/components/pages/Sidebar.jsx
+++ src/components/pages/Sidebar.jsx
@@ -1,36 +1,82 @@
-import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
-import AuthProvider from '../auth/AuthProvider';
-import Dashboard from './Dashboard';
-import Home from './Home';
-import Signup from '../auth/Signup';
-import PrivateRoute from '../auth/PrivateRoute';
-import Login from '../auth/Login';
+import React, { useRef, useEffect } from 'react';
+import { Route, Link, withRouter } from 'react-router-dom';
+import { gsap } from 'gsap';
+// import AuthProvider from '../auth/AuthProvider';
+// import Dashboard from './Dashboard';
+// import Home from './Home';
+// import Signup from '../auth/Signup';
+// import PrivateRoute from '../auth/PrivateRoute';
+// import Login from '../auth/Login';
 
 
-const Sidebar = () => {
+const Sidebar = props => {
+  const sidebarMenu = useRef(null);
+  const sidebarMenuOverlay = useRef(null);
+  const menuLayer = useRef(null);
+  const menuTimeline = useRef();
 
-    return <>
-    <div className="container">
-        <div className="row">
-          {/* MENU */}
-          <nav>
-            <Link to="/">Home</Link>
-            <Link to="/Signup">Signup</Link>
-            <Link to="/Login">Login</Link>
+  const { menuState, setMenuState } = props;
+
+  useEffect(() => {
+    menuTimeline.current = gsap.timeline({ paused: true });
+    menuTimeline.current.fromTo(
+      [sidebarMenuOverlay, menuLayer, sidebarMenu],
+      {
+        duration: 0,
+        x: '100%'
+      },
+      {
+        duration: 0.75,
+        x: '0%',
+        ease: 'power3.inOut',
+        stagger: {
+          amount: 0.5
+        }
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    menuState ? menuTimeline.current.play() : menuTimeline.current.reverse();
+  }, [menuState]);
+
+  useEffect(() => {
+    const { history } = props;
+    history.listen(() => setMenuState(false));
+  });
+
+  return (
+  <>
+    <div className="sidebarNavOverlay"
+      ref={sidebarMenuOverlay}
+      onClick={() => setMenuState(false)}></div>
+        <div className="menu-wrapper">
+          <div className="menu-layer" ref={menuLayer}></div>
+ 
+          <nav className="sidebarNav" ref={sidebarMenu}>
+            <div className="sidebar-top">
+              <div className="link-wrapper">
+                <Link to="/">Home</Link>
+                <Link to="/Signup">Signup</Link>
+                <Link to="/Login">Login</Link>
+              </div>
+            </div>
+            <div className="sidebar-bottom"></div>
           </nav>
+
           {/* CONTENT */}
-          <div>
-            {/* <AuthProvider>
+          {/* <div>
+            <AuthProvider>
               <Route exact path="/" component={Home} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
-            </AuthProvider> */}
-          </div>
+            </AuthProvider>
+          </div> */}
         </div>
-      </div>
+        
     </>
+  )
 };
 
-export default Sidebar;
+export default withRouter(Sidebar);
